89342530fe30aa20c371feded27599fa513ade9c
JOSIPCOLIC <colic.josip5@gmail.com>
2023-11-21 19:46:39 +0000

    demo changes

---
+++ demo1.py
@@ -0,0 +1,10 @@
+print('dobro dosli u PyDev program obrazovanja!')
+
+first_name = 'Pero'
+last_name = 'Peric'
+date_of_birth = 1998
+mobile_phone = '+385995897584'
+employed = True
+height = 180.99
+
+print(first_name, last_name)
